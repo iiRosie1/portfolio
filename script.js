40b2929d65af08bf5eec536478df5cd3803b7976
rosechen127 <rosechen127@gmail.com>
2025-04-19 17:10:29 -0400

    Added basic files

--- script.js
+++ script.js
@@ -14,4 +14,78 @@ document.addEventListener("DOMContentLoaded", () => {
         document.getElementById("footer").innerHTML = data;
       });
   });
+
+// Gradient interaction
+  document.addEventListener("mousemove", (e) => {
+    const hero = document.querySelector(".hero");
+    const x = (e.clientX / window.innerWidth) * 100;
+    const y = (e.clientY / window.innerHeight) * 100;
+    hero.style.setProperty("--x", `${x}%`);
+    hero.style.setProperty("--y", `${y}%`);
+  });
+
+  //View My Work button
+  document.querySelector('.hero-button').addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector('#projects').scrollIntoView({ 
+      behavior: 'smooth' 
+    });
+  });
+
+  //Typing
+  class TxtType {
+    constructor(el, toRotate, period) {
+      this.toRotate = toRotate;
+      this.el = el;
+      this.loopNum = 0;
+      this.period = parseInt(period, 10) || 2000;
+      this.txt = '';
+      this.isDeleting = false;
+      this.tick();
+    }
+  
+    tick() {
+      const i = this.loopNum % this.toRotate.length;
+      const fullTxt = this.toRotate[i];
+  
+      this.txt = this.isDeleting
+        ? fullTxt.substring(0, this.txt.length - 1)
+        : fullTxt.substring(0, this.txt.length + 1);
+  
+      this.el.querySelector('.wrap').innerHTML = this.txt;
+  
+      let delta = 150 - Math.random() * 100;
+  
+      if (this.isDeleting) { delta /= 2; }
+  
+      if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+      } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+      }
+  
+      setTimeout(() => this.tick(), delta);
+    }
+  }
+  
+  window.onload = function () {
+    const elements = document.getElementsByClassName('typewrite');
+    for (let i = 0; i < elements.length; i++) {
+      const toRotate = elements[i].getAttribute('data-type');
+      const period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtType(elements[i], JSON.parse(toRotate), period);
+      }
+    }
+  
+    // Cursor effect
+    const css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff }";
+    document.body.appendChild(css);
+  };
+
   
