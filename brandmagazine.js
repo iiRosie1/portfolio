224162f93f154dcd904bc9892eb79b06f0eb35ab
rosechen127 <rosechen127@gmail.com>
2025-04-25 15:31:39 -0400

    Added brandmagazine page

---
+++ brandmagazine.js
@@ -0,0 +1,17 @@
+// Load navbar and footer into every page
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("navbar.html")
+      .then((res) => res.text())
+      .then((data) => {
+        document.getElementById("navbar").innerHTML = data;
+      });
+  
+    fetch("footer.html")
+      .then((res) => res.text())
+      .then((data) => {
+        document.getElementById("footer").innerHTML = data;
+      });
+  });
+
+
+  
